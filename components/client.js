241d57424433918e361f3b8c17816e2a57ad23f8
Eric Martindale <eric@ericmartindale.com>
2019-11-03 15:52:37 -0800

    Snapshot of coverage, update client

--- components/client.js
+++ components/client.js
@@ -1,18 +1,30 @@
 'use strict';
 
-const Component = require('@fabric/http/components/component');
-const Canvas = require('./canvas');
+// Internal Types
+const RPG = require('../');
+const Canvas = require('../types/canvas');
+const Component = require('@fabric/http/types/component');
 
 class LiteClient extends Component {
   constructor (settings = {}) {
+    super(settings);
+
     this.settings = Object.assign({
       handle: 'rpg-lite-client'
     }, settings);
+
     this.canvas = new Canvas();
+    this.rpg = new RPG(this.settings);
+
+    this._state = {};
   }
 
   _getInnerHTML () {
-    return `<div id="rpg-lite-client"><canvas></canvas></div>`;
+    return `<div id="rpg-lite-client"><h1>hello game world!</h1><canvas></canvas></div>`;
+  }
+
+  async start () {
+    await this.rpg.start();
   }
 }
 
--- scripts/client.js
+++ scripts/client.js
@@ -1,13 +1,26 @@
 'use strict';
 
 const RPG = require('../');
+const Client = require('../components/client');
 const settings = require('../settings/default');
 
 async function main () {
-  window.rpg = new RPG(settings);
+  let element = document.querySelector('rpg-lite-client');
+
+  window.rpg = new Client(settings);
+  window.rpg.subscribe('/', _handleRootMessage.bind(this));
+
+  if (element) {
+    window.rpg._bind(element);
+  }
 
   await rpg.start();
   console.log('[RPG:LITE]', '[CLIENT]', 'Booted!  Now waiting for player input...');
+}
+
+async function _handleRootMessage (msg) {
+  // TODO: use Fabric.Router
+  console.warn('[RPG:LITE]', '[CLIENT]', 'Handling root message:', msg);
 }
 
 main();
