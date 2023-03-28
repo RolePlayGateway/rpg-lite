3c40c82eea90470d41eaec50ebf768a9d76bf9a0
Eric Martindale <eric@ericmartindale.com>
2023-03-28 07:31:20 -0400

    Use latest Fabric, update package-lock.json

--- scripts/node.js
+++ scripts/node.js
@@ -1,16 +1,13 @@
 'use strict';
 
 const RPG = require('../types/rpg');
-// const Swarm = require('@fabric/core/types/swarm');
 
 async function main () {
-  let rpg = new RPG({
+  const rpg = new RPG({
     authority: 'api.roleplaygateway.com:443'
   });
-  // let swarm = new Swarm();
 
   await rpg.start();
-  // await swarm.start();
 
   console.log('NODE STARTED! Data:', rpg);
   console.log('NODE STARTED! State:', rpg.state);
